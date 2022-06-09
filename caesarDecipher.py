4fb17434fd638c58c5c6fd99f87c2898b38ab038
mdalprato-arteco <mdalprato@arteco-global.com>
2022-06-09 15:43:09 +0200

    Fix

--- caesarDecipher.py
+++ caesarDecipher.py
@@ -1,10 +1,8 @@
 # caesar cipher decipher
 # info here https://en.wikipedia.org/wiki/Caesar_cipher
 
-
-# This is the decypher code
-
 def decipher_message(msg, attempt):
+# This is the decypher code
 
     LETTERS_LIST = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
                     "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
