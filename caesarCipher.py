f32ca9fe14ce14d248fad6de8c4a260897ecda62
mdalprato-arteco <mdalprato@arteco-global.com>
2022-06-09 13:41:05 +0200

    Added caesar cipher

---
+++ caesarCipher.py
@@ -0,0 +1,9 @@
+#Basic caesar cipher
+
+message = "Hello World";
+LETTERS_LIST = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+# Iterate over the string
+for element in message:
+    print(element, end=' ')
+print("\n")
