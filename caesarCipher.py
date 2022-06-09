4c3335e32a32402cbbe4341d2812ef8a98906ede
mdalprato-arteco <mdalprato@arteco-global.com>
2022-06-09 14:07:59 +0200

    Working

--- caesarCipher.py
+++ caesarCipher.py
@@ -1,18 +1,30 @@
-#Basic caesar cipher
+# Basic caesar cipher
 import array as arr
 
-message = "Zorro";
-LETTERS_LIST = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-output = [];
-outputMsg = '';
+message = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG"
+LETTERS_LIST = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+                "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+output = []
+outputMsg = ''
 
 # Iterate over the string
 for element in message:
-    #print(element, end=' ');
-    currentIndex = LETTERS_LIST.index(element.upper());
-    shiftedIndex = currentIndex + 3;
-    newLetter = LETTERS_LIST[shiftedIndex]
-    #print("element = " + element + ", new = " + newLetter);
-    output.append(newLetter);
+
+    isSpace = False
+
+    if(element == ' '):
+        isSpace = True
+
+    if(isSpace == False):
+        currentIndex = LETTERS_LIST.index(element.upper())
+        shiftedIndex = currentIndex - 3
+        lenghtOfLetters = len(LETTERS_LIST)
+        if(shiftedIndex > lenghtOfLetters):
+            shiftedIndex = shiftedIndex - lenghtOfLetters
+        newLetter = LETTERS_LIST[shiftedIndex];
+    else:
+        newLetter = ' ';
+
+    output.append(newLetter)
 
 print("Cipher message =  " + outputMsg.join(output))
