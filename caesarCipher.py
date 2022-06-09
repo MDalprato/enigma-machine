14465bb86f612e3cf07db71235e044b4cbeaec54
Marco Dalprato <5076292+MDalprato@users.noreply.github.com>
2022-06-09 18:27:53 +0200

    Fix uppercase for the cipher function

--- caesarCipher.py
+++ caesarCipher.py
@@ -1,7 +1,7 @@
 # Basic caesar cipher
 # info here https://en.wikipedia.org/wiki/Caesar_cipher
 
-message = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG !"
+message = "PROSSIMO attacco posizione 44.281525888115794, 11.900987295767901"
 LETTERS_LIST = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
                 "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 output = []
@@ -12,7 +12,7 @@ def cipher_message(msg):
     lenghtOfLetters = len(LETTERS_LIST);
     # Iterate over the string
     for element in msg:
-        if (element in LETTERS_LIST):
+        if (element.upper() in LETTERS_LIST):
             currentIndex = LETTERS_LIST.index(element.upper());
             shiftedIndex = currentIndex - 3;
             if(shiftedIndex > lenghtOfLetters):
