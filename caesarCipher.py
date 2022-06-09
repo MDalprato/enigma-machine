f12b42388083b72e2f8eb5f8be31ff822e3e01e5
mdalprato-arteco <mdalprato@arteco-global.com>
2022-06-09 13:47:29 +0200

    First step

--- caesarCipher.py
+++ caesarCipher.py
@@ -1,9 +1,14 @@
 #Basic caesar cipher
+import array as arr
 
-message = "Hello World";
+message = "Hello";
 LETTERS_LIST = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 # Iterate over the string
 for element in message:
-    print(element, end=' ')
+    print(element, end=' ');
+    currentIndex = LETTERS_LIST.index(element.upper());
+    shiftedIndex = currentIndex + 3;
+    newLetter = LETTERS_LIST[shiftedIndex]
+    print("element = " + element + ", new = " + newLetter);
 print("\n")
