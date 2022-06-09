87760ca6571f737ea9b86b8a9e819ae221b68ac6
mdalprato-arteco <mdalprato@arteco-global.com>
2022-06-09 13:57:18 +0200

    Working but bugged if index is too big

--- caesarCipher.py
+++ caesarCipher.py
@@ -1,14 +1,18 @@
 #Basic caesar cipher
 import array as arr
 
-message = "Hello";
+message = "Zorro";
 LETTERS_LIST = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+output = [];
+outputMsg = '';
 
 # Iterate over the string
 for element in message:
-    print(element, end=' ');
+    #print(element, end=' ');
     currentIndex = LETTERS_LIST.index(element.upper());
     shiftedIndex = currentIndex + 3;
     newLetter = LETTERS_LIST[shiftedIndex]
-    print("element = " + element + ", new = " + newLetter);
-print("\n")
+    #print("element = " + element + ", new = " + newLetter);
+    output.append(newLetter);
+
+print("Cipher message =  " + outputMsg.join(output))
