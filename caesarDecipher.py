e9fd5cbdb2d8670a31dcadb5e56aa4e87bc920a0
mdalprato-arteco <mdalprato@arteco-global.com>
2022-06-09 15:16:09 +0200

    Attempted reached

--- caesarDecipher.py
+++ caesarDecipher.py
@@ -1,24 +1,29 @@
-# Basic caesar cipher
+# caesar cipher decipher
 # info here https://en.wikipedia.org/wiki/Caesar_cipher
 
+secret_message = "QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD !"
 
-message = "QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD !"
 LETTERS_LIST = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
                 "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-output = []
-outputMsg = ''
+
+max_shift_attampt = 4; # this is how deep will be the attempt in order to decipher the message
 
 # Iterate over the string
-for element in message:
-    if (element in LETTERS_LIST):
-        currentIndex = LETTERS_LIST.index(element.upper());
-        shiftedIndex = currentIndex - 3;
-        lenghtOfLetters = len(LETTERS_LIST);
-        if(shiftedIndex > lenghtOfLetters):
-            shiftedIndex = shiftedIndex - lenghtOfLetters;
-        newLetter = LETTERS_LIST[shiftedIndex];
-    else:
-        newLetter = element;
-    output.append(newLetter)
 
-print("Cipher message =  " + outputMsg.join(output))
+for i in range(max_shift_attampt):
+
+  output = [];
+  outputMsg = '';
+  for element in secret_message:
+      if (element in LETTERS_LIST):
+          currentIndex = LETTERS_LIST.index(element.upper());
+          shiftedIndex = currentIndex + i;
+          lenghtOfLetters = len(LETTERS_LIST);
+          if(shiftedIndex >= lenghtOfLetters):
+              shiftedIndex = shiftedIndex - lenghtOfLetters;
+          newLetter = LETTERS_LIST[shiftedIndex];
+      else:
+          newLetter = element;
+      output.append(newLetter)
+      print("Cipher secret_message =  " + outputMsg.join(output))
+
